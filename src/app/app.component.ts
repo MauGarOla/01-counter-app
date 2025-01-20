512901661f34243c14fcbcab9e52060f6c44c867
MauGarOla <mau.gar.ola@hotmail.com>
2025-01-20 22:50:33 +0100

    Angular-Challenges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>User profile</h1>
+    <p>This is the user profile page</p>
+  `,
 })
 export class AppComponent {
   title = '01-counter-app';
